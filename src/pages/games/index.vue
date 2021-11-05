ff827b9289de61037db0817fd00b32da35f1712e
Santosh Bhandari <contact@santoshb.com.np>
2021-11-05 09:19:24 +0545

    feat: Add details to game listing page
    Added details like difficult and description and styled the page a little

--- src/pages/games/index.vue
+++ src/pages/games/index.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
+    <div class="flex flex-wrap">
         <div
-            v-for="(game, i) of games"
-            :key="i"
-            class="rounded-sm bg-gray-100 shadow-md px-3 py-1 my-2"
+            v-for="game of games"
+            :key="game.id"
+            class="w-full md:w-1/2"
         >
-            <NuxtLink :to="game.url">
-                <h2 class="text-xl font-semibold" > {{ game.title }} </h2>
-            </NuxtLink>
+            <div class="m-4 bg-gray-100 sh rounded-sm shadow-md px-3 py-2">
+                <NuxtLink :to="`/games/${game.id}`">
+                    <h2 class="text-2xl font-semibold" > {{ game.title }} </h2>
+                    <div class="mt-3 mb-6">
+                        <p
+                            v-for="(diff, i) of game.difficulty"
+                            :key="i"
+                            class="my-1 mr-2 px-2 py-1 rounded-full text-sm capitalize inline shadow"
+                            :class="diff"
+                        >
+                            {{ diff }}
+                        </p>
+                    </div>
+                    <p>
+                        {{ game.description }}
+                    </p>
+                </NuxtLink>
+            </div>
         </div>
     </div>
 </template>
@@ -19,16 +34,22 @@ export default defineComponent({
     setup() {
         const games = [
             {
+                title: "Guess that pokemon",
+                id: "guess-that-pokemon",
+                description: 'The classic "Who is that Pokemon?"',
+                difficulty: ["easy", "medium"]
+            },
+            {
                 title: "Name all pokemon by Generations",
-                url: "/games/name-by-generation"
+                id: "name-by-generation",
+                description: "Kanto to Galar, Try your best to name them all by the region they originated from!",
+                difficulty: ["medium", "hard"]
             },
             {
                 title: "Name all pokemon by Types",
-                url: "/games/name-by-type"
-            },
-            {
-                title: "Guess that pokemon",
-                url: "/games/guess-that-pokemon"
+                id: "name-by-type",
+                description: "Beautify fairy types or fierce dragon types, Try your best to name them all by the their types!",
+                difficulty: ["medium", "hard"]
             }
         ]
         return {
@@ -37,3 +58,15 @@ export default defineComponent({
     },
 })
 </script>
+
+<style lang="postcss" scoped>
+.easy {
+    @apply bg-sky-300;
+}
+.medium {
+    @apply bg-emerald-300;
+}
+.hard {
+    @apply bg-red-300;
+}
+</style>
